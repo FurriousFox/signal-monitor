ca93f14efb152080d2e74c065607ba7bf1aff66a
Micha <micha@argv.nl>
2025-10-19 20:06:20 +0200

    damn this is pretty complex, i thought this would be easy

---
+++ src/app/frontend/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default () => {
+    return (
+        <div>
+            <h1>hello world</h1>
+
+            <button className="bg-blue-500 m-3 p-3 rounded hover:bg-blue-700 text-white">test</button>
+
+            <Button>test</Button>
+        </div>
+    );
+
+};
